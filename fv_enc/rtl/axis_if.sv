792cd4ee0c4d539376c68b64236ed7db2c27c44e
Purush <emailpurush@gmail.com>
2024-05-06 22:57:40 +0200

    adding missing axis_if.sv file

---
+++ fv_enc/rtl/axis_if.sv
@@ -0,0 +1,11 @@
+interface axis_if #(parameter int DW =4);
+
+    logic  vld;
+    logic  last;
+    logic [DW-1:0] data;    
+    logic  rdy;
+
+  modport in ( input vld, data, last,     output rdy);
+  modport out ( output vld, data, last,   input rdy);
+ 
+endinterface : axis_if
