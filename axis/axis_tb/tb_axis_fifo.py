6f73447a728624605fd5443cc0d203ae3dfa7fbd
Purush <emailpurush@gmail.com>
2024-05-28 16:08:27 +0200

    adding backpressure

--- axis/axis_tb/tb_axis_fifo.py
+++ axis/axis_tb/tb_axis_fifo.py
@@ -6,6 +6,7 @@ from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles,NextTimeStep, E
 from cocotbext.axi import (AxiStreamBus, AxiStreamSource, AxiStreamFrame,AxiStreamSink)
 import logging
 import numpy as np
+import itertools
 
 
 class TB:
@@ -14,7 +15,7 @@ class TB:
        
         self.dut = dut
         self.set_logger(logger)
-        self.print_dut_info()
+        self.print_dut_attr()
 
         self.dut_rslt = []
 
@@ -29,13 +30,13 @@ class TB:
     def set_logger(self,logger):
         self.log = logging.getLogger(logger)
         self.log.setLevel(logging.INFO)
-        logging.getLogger(f"cocotb.{logger}").setLevel(logging.ERROR) ## Disable cocotb's built in logging
+        logging.getLogger(f"cocotb.{logger}").setLevel(logging.WARNING) ## Disable cocotb's built in logging
         log_filehandler = logging.FileHandler(f'build/{logger}.log')
         self.log.addHandler(log_filehandler)
 
-    def print_dut_info(self):
+    def print_dut_attr(self):
         self.log.info("Got DUT: {}".format(self.dut))
-        # self.log.info("DATA_W {}".format(dir(self.dut))) # displays signals and instances in dut
+        self.log.debug("DATA_W {}".format(dir(self.dut))) # displays signals and instances in dut
 
 
     async def reset_routine(self):
@@ -47,16 +48,25 @@ class TB:
     async def send_data(self, seq_id: int):
         self.data_in = np.random.randint(0,(2**8) -1, size=(1,10), dtype='uint8')
         self.log.info(f"{self.data_in[0] = }")
-        # self.data_in = np.array([246, 10, 84, 13], dtype='uint8')
         frame_in = AxiStreamFrame(b''.join(self.data_in[0]), tx_complete=Event())
+        pause_lst = [0,0,0,0,0,1,1,0]#np.random.randint(0,2, size=(1,10), dtype='uint8').tolist()
+        self.backpressure(pause_lst, "tx")
         await cocotb.start_soon(self.axis_src.send(frame_in))
         await cocotb.start_soon(self.collect_data(seq_id))
         await ClockCycles(self.dut.clk, 1) # wait for 1 rising edges
         self.log.info(f"@{get_sim_time(units='ns')} DONE: Sending msg frame ...")
         # start result capture
+
+    def backpressure(self, pause_list: list, chan:str ="tx"):
+        if chan == "rx":
+            self.axis_sink.set_pause_generator(itertools.cycle(pause_list))
+        else:
+            self.axis_src.set_pause_generator(itertools.cycle(pause_list))
         
 
-    async def collect_data(self, seq_id): 
+    async def collect_data(self, seq_id):
+        pause_lst = [0,1,1,1,0,0,0,0]
+        self.backpressure(pause_lst,"rx")
         rx = await self.axis_sink.recv()
         self.log.info(f"@{get_sim_time(units='ns')} DONE: recving msg frame ...")
         self.dut_rslt.append(rx.tdata)
